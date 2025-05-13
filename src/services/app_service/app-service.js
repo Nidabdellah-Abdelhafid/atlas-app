9d315f6172853d916ed97d0eede678c931bdad59
Nidabdellah-Abdelhafid <nidabdellahabdelhafid99@gmail.com>
2025-05-13 14:25:01 +0100

    13/05 new commit

--- src/services/app_service/app-service.js
+++ src/services/app_service/app-service.js
@@ -16,31 +16,5 @@ export class AppService {
       throw error;
     }
   }
-
-  async create(resource) {
-    try {
-      const response = await axios.post(this.url, resource);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async update(resource) {
-    try {
-      const response = await axios.put(`${this.url}/${resource.id}`, resource);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  async delete(resource) {
-    try {
-      const response = await axios.delete(`${this.url}/${resource.id}`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
+  
 }
